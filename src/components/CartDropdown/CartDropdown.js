3f10e1262f25f76ed9c25b73ff4aa06928749f73
Francis Ngo <francis.t.ngo@gmail.com>
2023-05-06 17:42:40 -0700

    Add type-safe to svg icons

--- src/components/CartDropdown/CartDropdown.js
+++ src/components/CartDropdown/CartDropdown.js
@@ -10,7 +10,7 @@ import {
 	CartDropdownContainer,
 	EmptyMessage,
 	CartItems,
-} from "./CartDropdown.styles.js";
+} from "./CartDropdown.styles";
 
 const CartDropdown = () => {
 	const cartItems = useSelector(selectCartItems);
--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -3,13 +3,13 @@ import {
 	selectIsCartOpen,
 	selectCartCount,
 } from "../../store/cart/cart.selector";
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { setIsCartOpen } from "../../store/cart/cart.action";
 
 import {
 	CartIconContainer,
 	ItemCount,
-	ShoppingIcon,
-} from "./CartIcon.styles.js";
+} from "./CartIcon.styles";
 
 const CartIcon = () => {
 	const dispatch = useDispatch();
