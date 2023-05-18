8dbe99363ea683bc9e39a0c5196dcfc57b21076a
Francis Ngo <francis.t.ngo@gmail.com>
2023-05-18 14:57:34 -0700

    Clean up server code

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,36 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import http from 'http';
+import bodyParser from 'body-parser';
 import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
 import typeDefs from './schemas/categories.typeDefs.js';
 import resolvers from './resolvers/categories.resolver.js'
 
 const app = express();
-const httpServer = http.createServer(app);
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
-});
+(async () => {
+    const httpServer = http.createServer(app);
 
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-});
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+    });
 
-console.log(`🚀  Server ready at: ${url}`);
+    await server.start();
+    app.use(
+        '/graphql',
+        morgan('dev'),
+        cors(),
+        bodyParser.json(),
+        expressMiddleware(server),
+      );
+
+    await new Promise<void>((resolve) =>
+        httpServer.listen({ port: 4000 }, resolve)
+    );
+    console.log(`🚀 Server ready at http://localhost:4000`);
+})();
