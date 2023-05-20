1c9859df11a4521351946025dc44d64a3d4cd9b7
Francis Ngo <francis.t.ngo@gmail.com>
2023-05-19 17:08:42 -0700

    Convert UserContext to TypeScript

---
+++ client/src/contexts/UserContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect, ReactNode, Dispatch, SetStateAction } from "react";
+import { User as FirebaseUser } from 'firebase/auth';
+import {
+	createUserDocumentFromAuth,
+	onAuthStateChangedListener,
+} from "../utils/firebase/firebase.utils";
+
+interface User {
+	uid: string;
+	email: string;
+	displayName: string;
+}
+
+interface UserContextType {
+	currentUser: User | null;
+	setCurrentUser: Dispatch<SetStateAction<User | null>>
+}
+
+interface UserProviderProps {
+	children: ReactNode;
+}
+
+export const UserContext = createContext<UserContextType>({
+	currentUser: null,
+	setCurrentUser: () => null,
+});
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+	const [currentUser, setCurrentUser] = useState<User | null>(null);
+	const value: UserContextType = { currentUser, setCurrentUser };
+
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user: FirebaseUser | null) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			setCurrentUser(user as User | null);
+		});
+		return unsubscribe;
+	}, []);
+
+	return (
+		<UserContext.Provider value={value}>{children}</UserContext.Provider>
+	);
+};
